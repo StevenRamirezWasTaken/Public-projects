0d731cc80894d4cb0cd920b0c1061c8b07669e72
Steven Ramirez <StevenRamirezWasTaken@gmail.com>
2023-10-07 00:13:28 +0800

    Create 4U.py

---
+++ 4U.py
@@ -0,0 +1,10 @@
+choices = ["I Love You Too!", "k", "T_T", "fuck u", "idc", ""]
+while True:
+	print("I love u")
+	reply = input(": ")
+	if reply == choices[0]:
+		print("The End!")
+		break
+	else:
+		print(":/")
+		continue
